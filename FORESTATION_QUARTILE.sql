db269994bf21a48acf60a53840f581e219aba024
Kelvin-M-Njoki <123241882+Kelvin-M-Njoki@users.noreply.github.com>
2023-02-22 15:35:44 +0300

    FORESTATION_QUARNTILE.SL

---
+++ FORESTATION_QUARTILE.sql
@@ -0,0 +1,23 @@
+--quantiles
+SELECT CASE WHEN per_forest<=25 THEN 'Q1'
+ WHEN per_forest<=50 THEN 'Q2'
+ WHEN per_forest<=75 THEN 'Q3'
+ WHEN per_forest>75 THEN 'Q4'
+ END AS forestation_quantile, COUNT(country_name)
+FROM forestation
+WHERE year=2016 AND country_name != 'World' AND per_forest IS NOT NULL
+GROUP BY forestation_quantile;
+
+
+--countries in the top quantile
+SELECT CASE WHEN per_forest<=25 THEN 'Q1'
+ WHEN per_forest<=50 THEN 'Q2'
+ WHEN per_forest<=75 THEN 'Q3'
+ WHEN per_forest>75 THEN 'Q4'
+ END AS forestation_quantile,
+ country_name,
+ region,
+ ROUND((per_forest::decimal),2) 
+FROM forestation
+WHERE year=2016 AND per_forest>75
+ORDER BY per_forest
